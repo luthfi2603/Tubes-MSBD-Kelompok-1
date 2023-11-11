9221e466b5c5607b829b0fe022efe72eada1d901
Rifqi <rifqijabrah@gmail.com>
2023-11-11 23:06:57 +0700

    nambah tabel log

---
+++ database/migrations/2023_11_11_154628_create_log_mahasiswas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void {
+        Schema::create('log_mahasiswas', function (Blueprint $table) {
+            $table->char('nim', 9)->primary();
+            $table->string('nama');
+            $table->char('angkatan', 4);
+            $table->enum('jenis_kelamin', ['P', 'L']);
+            $table->string('foto');
+            $table->enum('status', ['aktif','tidak_aktif', 'lulus']);
+            $table->integer('user_id');
+            $table->char('kode_prodi', 2);
+            $table->enum('action', ['insert','update', 'delete']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void {
+        Schema::dropIfExists('log_mahasiswas');
+    }
+};
