dcb87d9a3806eabdb6974547f1e5596ddd5ec555
Rifqi <rifqijabrah@gmail.com>
2023-11-16 22:12:17 +0700

    stash

--- database/migrations/2023_11_11_152219_create_ebooks_table.php
+++ database/migrations/2023_11_11_152219_create_ebooks_table.php
@@ -22,19 +22,19 @@ return new class extends Migration {
         DB::unprepared('
             CREATE TRIGGER log_ebooks_insert AFTER INSERT ON `ebooks` FOR EACH ROW
             BEGIN
-                INSERT INTO log_ebooks VALUES (NEW.id NEW.judul, NEW.penulis, NEW.url_file, NEW.tgl_terbit, NEW.diupload_oleh, "INSERT", NULL);
+                INSERT INTO log_ebooks VALUES (NEW.id, NEW.judul, NEW.penulis, NEW.url_file, NEW.tgl_terbit, NEW.diupload_oleh, "INSERT", NULL);
             END
         ');
         DB::unprepared('
             CREATE TRIGGER log_ebooks_update AFTER UPDATE ON `ebooks` FOR EACH ROW
             BEGIN
-                INSERT INTO log_ebooks VALUES (NEW.id NEW.judul, NEW.penulis, NEW.url_file, NEW.tgl_terbit, NEW.diupload_oleh, "UPDATE", NULL);
+                INSERT INTO log_ebooks VALUES (NEW.id, NEW.judul, NEW.penulis, NEW.url_file, NEW.tgl_terbit, NEW.diupload_oleh, "UPDATE", NULL);
             END
         ');
         DB::unprepared('
             CREATE TRIGGER log_ebooks_delete AFTER DELETE ON `ebooks` FOR EACH ROW
             BEGIN
-                INSERT INTO log_ebooks VALUES (OLD.id OLD.judul, OLD.penulis, OLD.url_file, OLD.tgl_terbit, OLD.diupload_oleh, "DELETE", NULL);
+                INSERT INTO log_ebooks VALUES (OLD.id, OLD.judul, OLD.penulis, OLD.url_file, OLD.tgl_terbit, OLD.diupload_oleh, "DELETE", NULL);
             END
         ');
 
