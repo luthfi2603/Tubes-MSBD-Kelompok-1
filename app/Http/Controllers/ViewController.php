9b7361bf33486d05d240acdd434d545cef71ad9d
luthfi2603 <luthfim904@gmail.com>
2023-12-22 17:25:18 +0700

    perbaiki procedure karya tulis dan detail karya tulis

--- app/Http/Controllers/ViewController.php
+++ app/Http/Controllers/ViewController.php
@@ -70,25 +70,11 @@ class ViewController extends Controller {
             ->where('id', $id)
             ->first();
 
-        $penulis = DB::table('view_karya_tulis')
+        $penuliss = DB::table('view_karya_tulis')
             ->select('kontributor', 'status')
             ->where('id', $id)
-            ->where('status', 'penulis')
             ->groupBy('kontributor', 'status')
-            ->get();
-
-        $pembimbing = DB::table('view_karya_tulis')
-            ->select('kontributor', 'status')
-            ->where('id', $id)
-            ->where('status', 'pembimbing')
-            ->groupBy('kontributor', 'status')
-            ->get();
-
-        $kontributor = DB::table('view_karya_tulis')
-            ->select('kontributor', 'status')
-            ->where('id', $id)
-            ->where('status', 'kontributor')
-            ->groupBy('kontributor', 'status')
+            ->orderBy('status')
             ->get();
 
         $kataKuncis = DB::table('view_karya_tulis')
@@ -103,7 +89,7 @@ class ViewController extends Controller {
         }
         $kataKunci = rtrim($kataKunci, ', ');
 
-        return view('detail-karya-tulis', compact('detail', 'kataKunci', 'penulis', 'pembimbing', 'kontributor', 'isLiked'));
+        return view('detail-karya-tulis', compact('detail', 'kataKunci', 'penuliss', 'isLiked'));
     }
 
     public function showEBook(Request $request){
